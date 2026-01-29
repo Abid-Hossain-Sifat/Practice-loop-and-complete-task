42c95e6e96e2f1e9a7478ccc32622de4fa066a57
Abid Hossain Sifat <itzabid006@gmail.com>
2026-01-29 20:38:15 +0600

    Solve a problem

--- Break task3.js
+++ Break task3.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <=100; i++){
     console.log(i)
     let mth = Math.sqrt(i);
-    if (Number.isInteger(mth)){
+    if (Number.isInteger(mth) && i !==1){
         console.log ('First square number is', i)
         break
      }
